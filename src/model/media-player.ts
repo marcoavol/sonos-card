655dbe67ccc55a89b1bca9ecbad01e1af0c2ae6a
Marco Volken <marco_volken@bluewin.ch>
2025-01-05 01:26:07 +0100

    Sort players in volumes section alphabetically to be consistent with other sections

--- src/model/media-player.ts
+++ src/model/media-player.ts
@@ -66,10 +66,12 @@ export class MediaPlayer {
   }
 
   private createGroupMembers(mainHassEntity: HassEntity, mediaPlayerHassEntities: HassEntity[]): MediaPlayer[] {
-    return getGroupPlayerIds(mainHassEntity).reduce((players: MediaPlayer[], id) => {
-      const hassEntity = mediaPlayerHassEntities.find((hassEntity) => hassEntity.entity_id === id);
-      return hassEntity ? [...players, new MediaPlayer(hassEntity, this.config)] : players;
-    }, []);
+    return getGroupPlayerIds(mainHassEntity)
+      .reduce((players: MediaPlayer[], id) => {
+        const hassEntity = mediaPlayerHassEntities.find((hassEntity) => hassEntity.entity_id === id);
+        return hassEntity ? [...players, new MediaPlayer(hassEntity, this.config)] : players;
+      }, [])
+      .sort((a, b) => a.id.localeCompare(b.id));
   }
 
   private determineVolumePlayer() {
